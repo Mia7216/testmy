b15a042cffcafca29357ca3d3a12fd37ad4d1435
Mehdi Imanpour <Mehdi.Imanpour@gmail.com>
2025-11-23 17:03:36 +0330

    ch 12-

--- articles/urls.py
+++ articles/urls.py
@@ -19,8 +19,11 @@ from django.urls import path, include
 
 from . import views
 
+
+app_name='articles'
 urlpatterns = [
 
-    path('',views.articles_list),
+    path('',views.articles_list, name="list"),
+    path('<slug>',views.article_detail,name="detail"),
 
 ]
